97cd63915a9e3672f6bd780506fb24ad74d38012
celine meunier <celinemeunier35@gmail.Com>
2016-05-12 15:03:12 +0200

    I had  create paterns

--- src/main/PatternManager.java
+++ src/main/PatternManager.java
@@ -9,13 +9,14 @@ public class PatternManager {
 	public void addLine(String line) {
 		lines.add(line);
 	}
-	
-	public String[] getLinesToArray() {
-		String[] array = new String[this.lines.size()];
-		for (int i = 0 ; i < this.lines.size(); i++) {
-			array[i] = this.lines.get(i);
-		}
-		return array;
+
+	/**
+	 * @return the lines
+	 */
+	public ArrayList<String> getLines() {
+		return lines;
 	}
+	
+	
 	
 }
