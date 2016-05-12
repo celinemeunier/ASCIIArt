3a077d9519eaca38a835d809cbe5f605e76a604b
valentin <renard.valentin@hotmail.fr>
2016-05-12 11:01:50 +0200

    Generate ASCIIText

---
+++ src/main/GameAscii.java
@@ -0,0 +1,47 @@
+package main;
+
+/**
+ * @author valentin
+ */
+public class GameAscii {
+    
+    public static String textToAscii(String[] param) {
+        Integer L = Integer.parseInt(param[0]);
+        Integer H = Integer.parseInt(param[1]);
+        String word = param[2].toUpperCase();
+        
+        char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        String[][] character = new String[alphabet.length + 1][H];
+        
+        //création des lettres
+        Integer nbLetter = 0;
+        for (int i = 0; i < L * (alphabet.length + 1); i += L) {
+            //par ligne
+            for (int j = 0; j < H; j++) {
+                character[nbLetter][j] = param[3 + j].substring(i, i + (L - 1));
+            }
+            nbLetter++;
+        }
+        
+        char[] tabWord = word.toCharArray();
+        //pour chaque ligne
+        for (int l = 0; l < H; l++ ) {
+            //pour chaque lettre
+            for (int i = 0; i < tabWord.length; i++) {
+                //recherche de lettre
+                Integer index = 26;
+                for (int j = 0; j < alphabet.length; j++) {
+                    if (tabWord[i] == alphabet[j]) {
+                        index = j;
+                        break;
+                    }
+                }
+                //ecriture de la lettre pour cette ligne
+                System.out.print(character[index][l] + " ");
+            }
+            System.out.print("\n");
+        }
+        return "";
+    }
+    
+}
---
+++ src/test/TestASCIIArt.java
@@ -0,0 +1,50 @@
+package test;
+
+import main.GameAscii;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author valentin
+ */
+public class TestASCIIArt {
+    
+    private String[] generate(Integer L, Integer H, String word) {
+        String[] input = new String[3 + L + H];
+        input[0] = L.toString();
+        input[1] = H.toString();
+        input[2] = word;
+        //letter
+        input[3] = " #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ";
+        input[4] = "# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ";
+        input[5] = "### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ";
+        input[6] = "# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ";
+        input[7] = "# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ";
+        return input;
+    }
+    
+    @Test
+    public void testLetterE() {
+        String[] param = this.generate(4, 5, "E");
+        GameAscii.textToAscii(param);
+    }
+    
+    @Test
+    public void testManhattan() {
+        String[] param = this.generate(4, 5, "MANHATTAN");
+        GameAscii.textToAscii(param);
+    }
+ 
+    @Test
+    public void testManhattanWithLowerCase() {
+        String[] param = this.generate(4, 5, "manhattan");
+        GameAscii.textToAscii(param);
+    }
+    
+    @Test
+    public void testManhattanWithNoLetter() {
+        String[] param = this.generate(4, 5, "M@NH@TT@N");
+        GameAscii.textToAscii(param);
+    }
+}
